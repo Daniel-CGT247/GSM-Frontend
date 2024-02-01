942576dd2ed9ef561b982e0d8558e57fd97b69c2
kevlei01001100 <156448718+kevlei01001100@users.noreply.github.com>
2024-02-01 18:52:26 +0000

    updated

--- src/pages/OperationLib.jsx
+++ src/pages/OperationLib.jsx
@@ -1,99 +1,198 @@
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
+// import Table from "react-bootstrap/Table";
+// import endpoint from "../utils/endpoint";
+
+// export default function OperationLibList({
+//   bundleGroup,
+//   listId,
+//   updateOperationLists,
+// }) {
+//   const [operationLibs, setOperationLibs] = useState([]);
+//   const [addedOperations, setAddedOperations] = useState([]);
+//   const headers = {
+//     Authorization: `JWT ${localStorage.getItem("access_token")}`,
+//   };
+
+//   const handleAddOperation = async (operation) => {
+//     try {
+//       const response = await axios.post(
+//         `${endpoint}/operation_list/`,
+//         {
+//           list: listId,
+//           operations: operation.id,
+//         },
+//         { headers }
+//       );
+
+//       // update added operations list
+//       setAddedOperations((prevAddedOperations) => [
+//         ...prevAddedOperations,
+//         operation.id,
+//       ]);
+
+//       console.log("Operation added successfully:", response.data);
+
+//       // update state directly without reloading the page
+//       setOperationLibs((prevOperationLibs) =>
+//         prevOperationLibs.map((op) =>
+//           op.id === operation.id ? { ...op, added: true } : op
+//         )
+//       );
+//       updateOperationLists();
+//     } catch (error) {
+//       console.error("Error adding operation:", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(`${endpoint}/operation_lib/`, {
+//           params: { bundle_group: bundleGroup },
+//         });
+//         setOperationLibs(response.data);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [bundleGroup, listId, updateOperationLists]);
+
+//   return (
+//     <Card>
+//       <Card.Header>
+//         <Card.Title>Operation Library</Card.Title>
+//       </Card.Header>
+//       <Card.Body>
+//         <Table striped hover>
+//           <thead>
+//             <tr>
+//               <th scope="col">Operation Code</th>
+//               <th scope="col">Name</th>
+//               {/* <th scope="col">Operated by</th> */}
+//               <th scope="col">Add</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {operationLibs &&
+//               operationLibs.map((operation) => (
+//                 <tr key={operation.id}>
+//                   <td>{operation.operation_code}</td>
+//                   <td>{operation.name}</td>
+//                   {/* <td>{operation.machine}</td> */}
+//                   <td>
+//                     <Button
+//                       className="btn-success"
+//                       onClick={() => handleAddOperation(operation)}
+//                     >
+//                       Add
+//                     </Button>
+//                   </td>
+//                 </tr>
+//               ))}
+//           </tbody>
+//         </Table>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
-import endpoint from "../utils/endpoint";
+import Card from "react-bootstrap/Card";
 
-export default function OperationLibList({
-  bundleGroup,
-  listId,
-  updateOperationLists,
+export default function OperationLibList({ 
+  bundleGroup, 
+  listId, 
+  updateOperationLists 
 }) {
   const [operationLibs, setOperationLibs] = useState([]);
-  const [addedOperations, setAddedOperations] = useState([]);
-  const headers = {
-    Authorization: `JWT ${localStorage.getItem("access_token")}`,
+  const headers = { 
+    Authorization: `JWT ${localStorage.getItem("access_token")}` 
+  };
+
+  useEffect(() => {
+    const storedOperationLibs = localStorage.getItem(`operationLibs_${bundleGroup}`);
+
+    if (storedOperationLibs) {
+      setOperationLibs(JSON.parse(storedOperationLibs));
+    } else {
+      fetchData(); 
+    }
+  }, [bundleGroup]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/operation_lib/", 
+        { 
+          params: { bundle_group: bundleGroup },
+          headers,
+      });
+      setOperationLibs(response.data);
+      localStorage.setItem(`operationLibs_${bundleGroup}`, JSON.stringify(response.data)); 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   const handleAddOperation = async (operation) => {
+    const updatedOperationLibs = operationLibs.map(op =>
+      op.id === operation.id ? { ...op, added: true } : op
+    );
+  
+    // Update local state and localStorage first for immediate UI update
+    setOperationLibs(updatedOperationLibs);
+    localStorage.setItem(`operationLibs_${bundleGroup}`, JSON.stringify(updatedOperationLibs));
+  
+    // Then, asynchronously update the backend
     try {
-      const response = await axios.post(
-        `${endpoint}/operation_list/`,
+      await axios.post(
+        "http://127.0.0.1:8000/operation_list/",
         {
           list: listId,
           operations: operation.id,
         },
         { headers }
       );
-
-      // update added operations list
-      setAddedOperations((prevAddedOperations) => [
-        ...prevAddedOperations,
-        operation.id,
-      ]);
-
-      console.log("Operation added successfully:", response.data);
-
-      // update state directly without reloading the page
-      setOperationLibs((prevOperationLibs) =>
-        prevOperationLibs.map((op) =>
-          op.id === operation.id ? { ...op, added: true } : op
-        )
-      );
-      updateOperationLists();
+  
+      // Optionally, confirm the operation was added and update UI as needed
+      updateOperationLists(); 
     } catch (error) {
       console.error("Error adding operation:", error);
+      // Handle error, possibly revert changes in UI if necessary
     }
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${endpoint}/operation_lib/`, {
-          params: { bundle_group: bundleGroup },
-        });
-        setOperationLibs(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [bundleGroup, listId, updateOperationLists]);
-
+  
   return (
     <Card>
-      <Card.Header>
-        <Card.Title>Operation Library</Card.Title>
-      </Card.Header>
+      <Card.Header><Card.Title>Operation Library</Card.Title></Card.Header>
       <Card.Body>
         <Table striped hover>
           <thead>
             <tr>
               <th scope="col">Operation Code</th>
               <th scope="col">Name</th>
-              {/* <th scope="col">Operated by</th> */}
               <th scope="col">Add</th>
             </tr>
           </thead>
           <tbody>
-            {operationLibs &&
-              operationLibs.map((operation) => (
-                <tr key={operation.id}>
-                  <td>{operation.operation_code}</td>
-                  <td>{operation.name}</td>
-                  {/* <td>{operation.machine}</td> */}
-                  <td>
-                    <Button
-                      className="btn-success"
-                      onClick={() => handleAddOperation(operation)}
-                    >
-                      Add
-                    </Button>
-                  </td>
-                </tr>
-              ))}
+            {operationLibs.map((operation) => (
+              <tr key={operation.id}>
+                <td>{operation.operation_code}</td>
+                <td>{operation.name}</td>
+                <td>
+                  <Button className="btn-success" onClick={() => handleAddOperation(operation)}>
+                    Add
+                  </Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Card.Body>
