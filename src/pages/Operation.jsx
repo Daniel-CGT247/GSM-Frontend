9cd91d9f4661ed9cd1b2f92068bb99991034d1a4
kevlei01001100 <156448718+kevlei01001100@users.noreply.github.com>
2024-02-06 01:06:40 +0000

    update

--- src/pages/Operation.jsx
+++ src/pages/Operation.jsx
@@ -1,164 +1,36 @@
-// import React, { useState } from "react";
-// import Button from "react-bootstrap/Button";
-// import { Link, useParams } from "react-router-dom";
-// import useGet from "../customed_hook/getData";
-// import OperationLibList from "./OperationLib";
-// import OperationList from "./OperationList";
-// import endpoint from "../utils/endpoint";
-
-// export default function Operation() {
-//   const { listId, jobId, bundleId } = useParams();
-
-//   const styleNum = useGet(`${endpoint}/collection/${listId}`);
-//   const itemName = styleNum && styleNum.item && styleNum.item.name;
-
-//   const jobGroup = useGet(`${endpoint}/job_group/${jobId}`);
-//   const bundle_group =
-//     jobGroup &&
-//     jobGroup.bundle_groups &&
-//     jobGroup.bundle_groups.find((bundle) => bundle.id == bundleId);
-//   const bundleName = bundle_group && bundle_group.name;
-//   // re-render OperationList
-//   const [operationListKey, setOperationListKey] = useState(0);
-
-//   // update OperationLists after an operation is deleted or added
-//   const updateOperationLists = (operationId, deleted) => {
-//     setOperationListKey((prevKey) => prevKey + 1);
-//   };
-
-//   return (
-//     <div className="p-5">
-//       <div className="flex flex-col space-y-2 items-center justify-center">
-//         <h1 className="font-bold">Build Operation - {bundleName}</h1>
-
-//         <h3 className="font-bold">Style {itemName}</h3>
-//         <div className="space-x-5">
-//           <Link to={`/${listId}/job_group`}>
-//             <Button variant="outline-secondary">Back to Job Group</Button>
-//           </Link>
-//           <Link to={`/${listId}/job_group/${jobId}/${bundleId}/your_list`}>
-//             <Button>Complete</Button>
-//           </Link>
-//         </div>
-//       </div>
-//       <div className="container text-center my-5">
-//         <div className="row">
-//           <div className="col">
-//             <OperationLibList
-//               bundleGroup={bundleId}
-//               listId={listId}
-//               updateOperationLists={updateOperationLists}
-//             />
-//           </div>
-//           <div className="col space-y-10">
-//             <OperationList
-//               bundleGroup={bundleId}
-//               listId={listId}
-//               key={operationListKey}
-//               updateOperationLists={updateOperationLists}
-//               bundleName={bundleName}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Link, useParams } from "react-router-dom";
 import useGet from "../customed_hook/getData";
-import OperationLibList from "./OperationLib"; // Correct the import path if needed
-import OperationList from "./OperationList"; // Correct the import path if needed
+import OperationLibList from "./OperationLib";
+import OperationList from "./OperationList";
 import endpoint from "../utils/endpoint";
 
 export default function Operation() {
   const { listId, jobId, bundleId } = useParams();
 
-  // Fetching style and job group information
   const styleNum = useGet(`${endpoint}/collection/${listId}`);
   const itemName = styleNum && styleNum.item && styleNum.item.name;
 
   const jobGroup = useGet(`${endpoint}/job_group/${jobId}`);
-  const bundle_group = jobGroup && jobGroup.bundle_groups && jobGroup.bundle_groups.find(bundle => bundle.id === bundleId);
+  const bundle_group =
+    jobGroup &&
+    jobGroup.bundle_groups &&
+    jobGroup.bundle_groups.find((bundle) => bundle.id == bundleId);
   const bundleName = bundle_group && bundle_group.name;
+  // re-render OperationList
+  const [operationListKey, setOperationListKey] = useState(0);
 
-  // States for operation libraries and lists
-  const [operationLibs, setOperationLibs] = useState([]);
-  const [operationList, setOperationList] = useState([]);
-
-  const [operations, setOperations] = useState({}); // Operations now keyed by listId
-
-
-  useEffect(() => {
-    // Fetch operation libraries
-    const fetchOperationLibs = async () => {
-      try {
-        const response = await axios.get(`${endpoint}/operation_lib/?bundle_group=${bundleId}`);
-        setOperationLibs(response.data);
-      } catch (error) {
-        console.error("Failed to fetch operation libraries:", error);
-      }
-    };
-
-    // Fetch operation list
-    // But when fetching operation lists, you should adjust it to store the data considering both listId and bundleGroup
-    const fetchOperationList = async () => {
-      try {
-        const response = await axios.get(`${endpoint}/operation_list/?list_id=${listId}&bundle_group=${bundleId}`, {
-            headers: {
-                Authorization: `JWT ${localStorage.getItem("access_token")}`,
-            },
-        });
-        // Assuming response.data is an array of operations
-        updateOperationList(bundleId, response.data); // Pass the whole array to be set in the state
-      } catch (error) {
-          console.error("Failed to fetch operation list:", error);
-      }
-    };
-
-
-    fetchOperationLibs();
-    fetchOperationList();
-  }, [listId, jobId, bundleId]); // Re-fetch when these parameters change
-
-// Update function for operation list
-// const updateOperationList = (operation, isDelete = false) => {
-//   setOperations(prev => {
-//       const listOperations = prev[listId] || [];
-//       if (isDelete) {
-//           return { ...prev, [listId]: listOperations.filter(op => op.id !== operation) };
-//       } else {
-//           return { ...prev, [listId]: [...listOperations, operation] };
-//       }
-//   });
-// };
-const updateOperationList = (bundleGroupId, operation, isDelete = false) => {
-  setOperations(prev => {
-    // Copy the current state to avoid direct mutation
-    const newState = { ...prev };
-
-    // Ensure the structure for current listId and bundleGroupId exists
-    if (!newState[listId]) newState[listId] = {};
-    if (!newState[listId][bundleGroupId]) newState[listId][bundleGroupId] = [];
-
-    // Add or delete the operation
-    if (isDelete) {
-      newState[listId][bundleGroupId] = newState[listId][bundleGroupId].filter(op => op.id !== operation.id);
-    } else {
-      newState[listId][bundleGroupId] = [...newState[listId][bundleGroupId], operation];
-    }
-
-    return newState;
-  });
-};
+  // update OperationLists after an operation is deleted or added
+  const updateOperationLists = (operationId, deleted) => {
+    setOperationListKey((prevKey) => prevKey + 1);
+  };
 
   return (
     <div className="p-5">
       <div className="flex flex-col space-y-2 items-center justify-center">
         <h1 className="font-bold">Build Operation - {bundleName}</h1>
+
         <h3 className="font-bold">Style {itemName}</h3>
         <div className="space-x-5">
           <Link to={`/${listId}/job_group`}>
@@ -172,25 +44,129 @@ const updateOperationList = (bundleGroupId, operation, isDelete = false) => {
       <div className="container text-center my-5">
         <div className="row">
           <div className="col">
-          <OperationLibList
-                operationLibs={operationLibs}
-                listId={listId}
-                token={localStorage.getItem("access_token")}
-                updateOperationList={(operation) => updateOperationList(operation, false)}
+            <OperationLibList
+              bundleGroup={bundleId}
+              listId={listId}
+              updateOperationLists={updateOperationLists}
             />
           </div>
           <div className="col space-y-10">
-          <OperationList
-  operationList={operations[listId] && operations[listId][bundleId] ? operations[listId][bundleId] : []}
-  token={localStorage.getItem("access_token")}
-  bundleGroupId={bundleId}
-  updateOperationList={(operationId, isDelete) => updateOperationList(bundleId, operationId, isDelete)}
-/>
-
-
+            <OperationList
+              bundleGroup={bundleId}
+              listId={listId}
+              key={operationListKey}
+              updateOperationLists={updateOperationLists}
+              bundleName={bundleName}
+            />
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Button from "react-bootstrap/Button";
+// import { Link, useParams } from "react-router-dom";
+// import useGet from "../customed_hook/getData";
+// import OperationLibList from "./OperationLib"; 
+// import OperationList from "./OperationList"; 
+// import endpoint from "../utils/endpoint";
+
+// export default function Operation() {
+//   const { listId, jobId, bundleId } = useParams();
+
+//   // Fetching style and job group information
+//   const styleNum = useGet(`${endpoint}/collection/${listId}`);
+//   const itemName = styleNum && styleNum.item && styleNum.item.name;
+
+//   const jobGroup = useGet(`${endpoint}/job_group/${jobId}`);
+//   const bundle_group = jobGroup && jobGroup.bundle_groups && jobGroup.bundle_groups.find(bundle => bundle.id === bundleId);
+//   const bundleName = bundle_group && bundle_group.name;
+
+//   // States for operation libraries and lists
+//   const [operationLibs, setOperationLibs] = useState([]);
+//   const [operationList, setOperationList] = useState([]);
+
+//   const [operations, setOperations] = useState({}); // Operations now keyed by listId
+
+
+//   useEffect(() => {
+//     // Fetch operation libraries
+//     const fetchOperationLibs = async () => {
+//       try {
+//         const response = await axios.get(`${endpoint}/operation_lib/?bundle_group=${bundleId}`);
+//         setOperationLibs(response.data);
+//       } catch (error) {
+//         console.error("Failed to fetch operation libraries:", error);
+//       }
+//     };
+
+//     // Fetch operation list
+//     const fetchOperationList = async () => {
+//       try {
+//           const response = await axios.get(`${endpoint}/operation_list/?list_id=${listId}&bundle_group=${bundleId}`, {
+//               headers: {
+//                   Authorization: `JWT ${localStorage.getItem("access_token")}`,
+//               },
+//           });
+//           setOperations(prev => ({ ...prev, [listId]: response.data }));
+//       } catch (error) {
+//           console.error("Failed to fetch operation list:", error);
+//       }
+//   };
+
+//     fetchOperationLibs();
+//     fetchOperationList();
+//   }, [listId, jobId, bundleId]); // Re-fetch when these parameters change
+
+// // Update function for operation list
+// const updateOperationList = (operation, isDelete = false) => {
+//   setOperations(prev => {
+//       const listOperations = prev[listId] || [];
+//       if (isDelete) {
+//           return { ...prev, [listId]: listOperations.filter(op => op.id !== operation) };
+//       } else {
+//           return { ...prev, [listId]: [...listOperations, operation] };
+//       }
+//   });
+// };
+
+//   return (
+//     <div className="p-5">
+//       <div className="flex flex-col space-y-2 items-center justify-center">
+//         <h1 className="font-bold">Build Operation - {bundleName}</h1>
+//         <h3 className="font-bold">Style {itemName}</h3>
+//         <div className="space-x-5">
+//           <Link to={`/${listId}/job_group`}>
+//             <Button variant="outline-secondary">Back to Job Group</Button>
+//           </Link>
+//           <Link to={`/${listId}/job_group/${jobId}/${bundleId}/your_list`}>
+//             <Button>Complete</Button>
+//           </Link>
+//         </div>
+//       </div>
+//       <div className="container text-center my-5">
+//         <div className="row">
+//           <div className="col">
+//           <OperationLibList
+//                 operationLibs={operationLibs}
+//                 listId={listId}
+//                 token={localStorage.getItem("access_token")}
+//                 updateOperationList={(operation) => updateOperationList(operation, false)}
+//             />
+//           </div>
+//           <div className="col space-y-10">
+//           <OperationList
+//                 operationList={operations[listId] || []}
+//                 token={localStorage.getItem("access_token")}
+//                 updateOperationList={(operationId) => updateOperationList(operationId, true)}
+//             />
+
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
--- src/pages/OperationList.jsx
+++ src/pages/OperationList.jsx
@@ -1,96 +1,96 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import Button from "react-bootstrap/Button";
-// import Card from "react-bootstrap/Card";
-// import Table from "react-bootstrap/Table";
-// import endpoint from "../utils/endpoint";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+import endpoint from "../utils/endpoint";
 
-// export default function OperationList({
-//   bundleGroup,
-//   listId,
-//   updateOperationLists,
-// }) {
-//   const [operationList, setOperationList] = useState([]);
+export default function OperationList({
+  bundleGroup,
+  listId,
+  updateOperationLists,
+}) {
+  const [operationList, setOperationList] = useState([]);
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(`${endpoint}/operation_list/`, {
-//           params: {
-//             bundle_group: bundleGroup,
-//             listId: listId,
-//           },
-//           headers: {
-//             Authorization: `JWT ${localStorage.getItem("access_token")}`,
-//           },
-//         });
-//         setOperationList(response.data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${endpoint}/operation_list/`, {
+          params: {
+            bundle_group: bundleGroup,
+            listId: listId,
+          },
+          headers: {
+            Authorization: `JWT ${localStorage.getItem("access_token")}`,
+          },
+        });
+        setOperationList(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-//     fetchData();
-//   }, [bundleGroup, listId]);
+    fetchData();
+  }, [bundleGroup, listId]);
 
-//   const handleDelete = (operationListId, operationId) => {
-//     axios
-//       .delete(`${endpoint}/operation_list/${operationListId}`, {
-//         headers: {
-//           Authorization: `JWT ${localStorage.getItem("access_token")}`,
-//         },
-//       })
-//       .then((response) => {
-//         const newList = operationList.filter(
-//           (item) => item.id !== operationListId
-//         );
-//         setOperationList(newList);
+  const handleDelete = (operationListId, operationId) => {
+    axios
+      .delete(`${endpoint}/operation_list/${operationListId}`, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((response) => {
+        const newList = operationList.filter(
+          (item) => item.id !== operationListId
+        );
+        setOperationList(newList);
 
-//         // notify Operation to update OperationLists
-//         updateOperationLists(operationId, false);
-//       })
-//       .catch((error) => {
-//         console.error("Error deleting data:", error);
-//       });
-//   };
+        // notify Operation to update OperationLists
+        updateOperationLists(operationId, false);
+      })
+      .catch((error) => {
+        console.error("Error deleting data:", error);
+      });
+  };
 
-//   return (
-//     <Card>
-//       <Card.Header>
-//         <Card.Title>Operation List</Card.Title>
-//       </Card.Header>
-//       <Card.Body>
-//         <Table striped hover>
-//           <thead>
-//             <tr>
-//               <th>#</th>
-//               <th>Operation Code</th>
-//               <th>Name</th>
-//               <th>Delete</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {operationList.map((item, index) => (
-//               <tr key={item.id}>
-//                 <td>{index + 1}</td>
-//                 <td>{item.operations.operation_code}</td>
-//                 <td>{item.operations.name}</td>
-//                 <td>
-//                   <Button
-//                     className="btn-danger"
-//                     onClick={() => handleDelete(item.id)}
-//                   >
-//                     Delete
-//                   </Button>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </Table>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title>Operation List</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Table striped hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Operation Code</th>
+              <th>Name</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {operationList.map((item, index) => (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>{item.operations.operation_code}</td>
+                <td>{item.operations.name}</td>
+                <td>
+                  <Button
+                    className="btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
+  );
+}
 // import React from 'react';
 // import Button from 'react-bootstrap/Button';
 // import Table from 'react-bootstrap/Table';
@@ -133,60 +133,60 @@
 // export default OperationList;
 
 
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
-import Card from 'react-bootstrap/Card';
-import axios from 'axios';
-import endpoint from "../utils/endpoint";
-const OperationList = ({ operationList, updateOperationList }) => {
+// import React from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Table from 'react-bootstrap/Table';
+// import Card from 'react-bootstrap/Card';
+// import axios from 'axios';
+// import endpoint from "../utils/endpoint";
+// const OperationList = ({ operationList, updateOperationList }) => {
 
-  const handleDelete = async (operationId) => {
-    try {
-      await axios.delete(`${endpoint}/operation_list/${operationId}`, {
-        headers: {
-          Authorization: `JWT ${localStorage.getItem("access_token")}`,
-        },
-      });
+//   const handleDelete = async (operationId) => {
+//     try {
+//       await axios.delete(`${endpoint}/operation_list/${operationId}`, {
+//         headers: {
+//           Authorization: `JWT ${localStorage.getItem("access_token")}`,
+//         },
+//       });
 
-      updateOperationList(operationId, true); // This function needs to be implemented in the parent component to update the state accordingly
-    } catch (error) {
-      console.error("Error deleting operation:", error);
-    }
-  };
+//       updateOperationList(operationId, true); // This function needs to be implemented in the parent component to update the state accordingly
+//     } catch (error) {
+//       console.error("Error deleting operation:", error);
+//     }
+//   };
   
 
-  return (
-    <Card>
-      <Card.Header>Operation List</Card.Header>
-      <Card.Body>
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Operation Code</th>
-              <th>Name</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {operationList.map((operation, index) => (
-              <tr key={operation.id}>
-                <td>{index + 1}</td>
-                <td>{operation.operations.operation_code}</td>
-                <td>{operation.operations.name}</td>
-                <td>
-                  <Button variant="danger" onClick={() => handleDelete(operation.id)}>
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Card.Body>
-    </Card>
-  );
-};
+//   return (
+//     <Card>
+//       <Card.Header>Operation List</Card.Header>
+//       <Card.Body>
+//         <Table striped hover>
+//           <thead>
+//             <tr>
+//               <th>#</th>
+//               <th>Operation Code</th>
+//               <th>Name</th>
+//               <th>Delete</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {operationList.map((operation, index) => (
+//               <tr key={operation.id}>
+//                 <td>{index + 1}</td>
+//                 <td>{operation.operations.operation_code}</td>
+//                 <td>{operation.operations.name}</td>
+//                 <td>
+//                   <Button variant="danger" onClick={() => handleDelete(operation.id)}>
+//                     Delete
+//                   </Button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </Table>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
 
-export default OperationList;
+// export default OperationList;
