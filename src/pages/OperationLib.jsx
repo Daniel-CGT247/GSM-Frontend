babc0f4123edb7be81dc11c90749abe24fcf2b06
kevlei01001100 <156448718+kevlei01001100@users.noreply.github.com>
2024-02-06 03:02:42 +0000

    update

--- src/pages/OperationLib.jsx
+++ src/pages/OperationLib.jsx
@@ -1,114 +1,51 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import Button from "react-bootstrap/Button";
-// import Card from "react-bootstrap/Card";
-// import Table from "react-bootstrap/Table";
-// import endpoint from "../utils/endpoint";
-
-// export default function OperationLibList({
-//   bundleGroup,
-//   listId,
-//   updateOperationLists,
-// }) {
-//   const [operationLibs, setOperationLibs] = useState([]);
-//   const [addedOperations, setAddedOperations] = useState([]);
-//   const headers = {
-//     Authorization: `JWT ${localStorage.getItem("access_token")}`,
-//   };
-
-//   const handleAddOperation = async (operation) => {
-//     try {
-//       const response = await axios.post(
-//         `${endpoint}/operation_list/`,
-//         {
-//           list: listId,
-//           operations: operation.id,
-//         },
-//         { headers }
-//       );
-
-//       // update added operations list
-//       setAddedOperations((prevAddedOperations) => [
-//         ...prevAddedOperations,
-//         operation.id,
-//       ]);
-
-//       console.log("Operation added successfully:", response.data);
-
-//       // update state directly without reloading the page
-//       setOperationLibs((prevOperationLibs) =>
-//         prevOperationLibs.map((op) =>
-//           op.id === operation.id ? { ...op, added: true } : op
-//         )
-//       );
-//       updateOperationLists();
-//     } catch (error) {
-//       console.error("Error adding operation:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(`${endpoint}/operation_lib/`, {
-//           params: { bundle_group: bundleGroup },
-//         });
-//         setOperationLibs(response.data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, [bundleGroup, listId, updateOperationLists]);
-
-//   return (
-//     <Card>
-//       <Card.Header>
-//         <Card.Title>Operation Library</Card.Title>
-//       </Card.Header>
-//       <Card.Body>
-//         <Table striped hover>
-//           <thead>
-//             <tr>
-//               <th scope="col">Operation Code</th>
-//               <th scope="col">Name</th>
-//               {/* <th scope="col">Operated by</th> */}
-//               <th scope="col">Add</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {operationLibs &&
-//               operationLibs.map((operation) => (
-//                 <tr key={operation.id}>
-//                   <td>{operation.operation_code}</td>
-//                   <td>{operation.name}</td>
-//                   {/* <td>{operation.machine}</td> */}
-//                   <td>
-//                     <Button
-//                       className="btn-success"
-//                       onClick={() => handleAddOperation(operation)}
-//                     >
-//                       Add
-//                     </Button>
-//                   </td>
-//                 </tr>
-//               ))}
-//           </tbody>
-//         </Table>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import endpoint from "../utils/endpoint";
 
-export default function OperationLibList({ bundleGroup, listId, updateOperationLists }) {
+export default function OperationLibList({
+  bundleGroup,
+  listId,
+  updateOperationLists,
+}) {
   const [operationLibs, setOperationLibs] = useState([]);
+  const [addedOperations, setAddedOperations] = useState([]);
+  const headers = {
+    Authorization: `JWT ${localStorage.getItem("access_token")}`,
+  };
+
+  const handleAddOperation = async (operation) => {
+    try {
+      const response = await axios.post(
+        `${endpoint}/operation_list/`,
+        {
+          list: listId,
+          operations: operation.id,
+        },
+        { headers }
+      );
+
+      // update added operations list
+      setAddedOperations((prevAddedOperations) => [
+        ...prevAddedOperations,
+        operation.id,
+      ]);
+
+      console.log("Operation added successfully:", response.data);
+
+      // update state directly without reloading the page
+      setOperationLibs((prevOperationLibs) =>
+        prevOperationLibs.map((op) =>
+          op.id === operation.id ? { ...op, added: true } : op
+        )
+      );
+      updateOperationLists();
+    } catch (error) {
+      console.error("Error adding operation:", error);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -116,11 +53,7 @@ export default function OperationLibList({ bundleGroup, listId, updateOperationL
         const response = await axios.get(`${endpoint}/operation_lib/`, {
           params: { bundle_group: bundleGroup },
         });
-        const operationsWithAddedStatus = response.data.map(op => ({
-          ...op,
-          added: !!getOperationFromLocalStorage(op.id),
-        }));
-        setOperationLibs(operationsWithAddedStatus);
+        setOperationLibs(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -128,41 +61,6 @@ export default function OperationLibList({ bundleGroup, listId, updateOperationL
 
     fetchData();
   }, [bundleGroup, listId, updateOperationLists]);
-
-  const handleAddOperation = (operation) => {
-    // Update local storage immediately for UI update
-    addOperationToLocalStorage(operation);
-    setOperationLibs((prevOperationLibs) =>
-      prevOperationLibs.map((op) =>
-        op.id === operation.id ? { ...op, added: true } : op
-      )
-    );
-    // Notify parent component to re-render the operation list
-    updateOperationLists();
-
-    // Asynchronously update the backend
-    const headers = { Authorization: `JWT ${localStorage.getItem("access_token")}` };
-    axios.post(`${endpoint}/operation_list/`, { list: listId, operations: operation.id }, { headers })
-      .then(response => {
-        console.log("Operation added successfully:", response.data);
-      })
-      .catch(error => {
-        console.error("Error adding operation:", error);
-      });
-  };
-
-  // Add operation to local storage for immediate UI reflection
-  const addOperationToLocalStorage = (operation) => {
-    let operations = JSON.parse(localStorage.getItem("operations") || "[]");
-    operations.push(operation);
-    localStorage.setItem("operations", JSON.stringify(operations));
-  };
-
-  // Check if operation is in local storage (used to set added status initially)
-  const getOperationFromLocalStorage = (operationId) => {
-    let operations = JSON.parse(localStorage.getItem("operations") || "[]");
-    return operations.find(op => op.id === operationId);
-  };
 
   return (
     <Card>
@@ -175,25 +73,27 @@ export default function OperationLibList({ bundleGroup, listId, updateOperationL
             <tr>
               <th scope="col">Operation Code</th>
               <th scope="col">Name</th>
+              {/* <th scope="col">Operated by</th> */}
               <th scope="col">Add</th>
             </tr>
           </thead>
           <tbody>
-            {operationLibs.map((operation) => (
-              <tr key={operation.id}>
-                <td>{operation.operation_code}</td>
-                <td>{operation.name}</td>
-                <td>
-                  <Button
-                    // className={operation.added ? "btn-secondary" : "btn-success"}
-                    onClick={() => handleAddOperation(operation)}
-                  //   disabled={operation.added}
-                   >
-                
-                  </Button>
-                </td>
-              </tr>
-            ))}
+            {operationLibs &&
+              operationLibs.map((operation) => (
+                <tr key={operation.id}>
+                  <td>{operation.operation_code}</td>
+                  <td>{operation.name}</td>
+                  {/* <td>{operation.machine}</td> */}
+                  <td>
+                    <Button
+                      className="btn-success"
+                      onClick={() => handleAddOperation(operation)}
+                    >
+                      Add
+                    </Button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </Table>
       </Card.Body>
--- src/pages/OperationList.jsx
+++ src/pages/OperationList.jsx
@@ -1,166 +1,85 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import Button from "react-bootstrap/Button";
-// import Card from "react-bootstrap/Card";
-// import Table from "react-bootstrap/Table";
-// import endpoint from "../utils/endpoint";
-
-// export default function OperationList({
-//   bundleGroup,
-//   listId,
-//   updateOperationLists,
-// }) {
-//   const [operationList, setOperationList] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(`${endpoint}/operation_list/`, {
-//           params: {
-//             bundle_group: bundleGroup,
-//             listId: listId,
-//           },
-//           headers: {
-//             Authorization: `JWT ${localStorage.getItem("access_token")}`,
-//           },
-//         });
-//         setOperationList(response.data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, [bundleGroup, listId]);
-
-//   const handleDelete = (operationListId, operationId) => {
-//     axios
-//       .delete(`${endpoint}/operation_list/${operationListId}`, {
-//         headers: {
-//           Authorization: `JWT ${localStorage.getItem("access_token")}`,
-//         },
-//       })
-//       .then((response) => {
-//         const newList = operationList.filter(
-//           (item) => item.id !== operationListId
-//         );
-//         setOperationList(newList);
-
-//         // notify Operation to update OperationLists
-//         updateOperationLists(operationId, false);
-//       })
-//       .catch((error) => {
-//         console.error("Error deleting data:", error);
-//       });
-//   };
-
-//   return (
-//     <Card>
-//       <Card.Header>
-//         <Card.Title>Operation List</Card.Title>
-//       </Card.Header>
-//       <Card.Body>
-//         <Table striped hover>
-//           <thead>
-//             <tr>
-//               <th>#</th>
-//               <th>Operation Code</th>
-//               <th>Name</th>
-//               <th>Delete</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {operationList.map((item, index) => (
-//               <tr key={item.id}>
-//                 <td>{index + 1}</td>
-//                 <td>{item.operations.operation_code}</td>
-//                 <td>{item.operations.name}</td>
-//                 <td>
-//                   <Button
-//                     className="btn-danger"
-//                     onClick={() => handleDelete(item.id)}
-//                   >
-//                     Delete
-//                   </Button>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </Table>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import endpoint from "../utils/endpoint";
 
-export default function OperationList({ bundleGroup, listId }) {
+export default function OperationList({
+  bundleGroup,
+  listId,
+  updateOperationLists,
+}) {
   const [operationList, setOperationList] = useState([]);
 
   useEffect(() => {
-    // Load operations from local storage
-    loadOperationsFromLocalStorage();
-  }, []); // Removed unnecessary dependencies to avoid re-fetching
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${endpoint}/operation_list/`, {
+          params: {
+            bundle_group: bundleGroup,
+            listId: listId,
+          },
+          headers: {
+            Authorization: `JWT ${localStorage.getItem("access_token")}`,
+          },
+        });
+        setOperationList(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  const loadOperationsFromLocalStorage = () => {
-    const operations = JSON.parse(localStorage.getItem("operations") || "[]");
-    // Assuming operations stored in local storage include bundleGroup info
-    const filteredOperations = operations.filter(op => op.bundleGroup === bundleGroup);
-    setOperationList(filteredOperations);
-  };
+    fetchData();
+  }, [bundleGroup, listId]);
 
-  const handleDelete = async (operationId) => {
-    // Optimistically remove the operation from UI
-    const updatedOperations = operationList.filter(op => op.id !== operationId);
-    setOperationList(updatedOperations);
+  const handleDelete = (operationListId, operationId) => {
+    axios
+      .delete(`${endpoint}/operation_list/${operationListId}`, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((response) => {
+        const newList = operationList.filter(
+          (item) => item.id !== operationListId
+        );
+        setOperationList(newList);
 
-    // Remove operation from local storage
-    removeOperationFromLocalStorage(operationId);
-
-    // Asynchronously delete from backend
-    try {
-      await axios.delete(`${endpoint}/operation_list/${operationId}`, {
-        headers: { Authorization: `JWT ${localStorage.getItem("access_token")}` },
+        // notify Operation to update OperationLists
+        updateOperationLists(operationId, false);
+      })
+      .catch((error) => {
+        console.error("Error deleting data:", error);
       });
-      console.log("Operation deleted successfully");
-    } catch (error) {
-      console.error("Error deleting operation:", error);
-      // If deletion fails, reload the operations from local storage or handle the error appropriately
-      loadOperationsFromLocalStorage();
-    }
-  };
-
-  const removeOperationFromLocalStorage = (operationId) => {
-    let operations = JSON.parse(localStorage.getItem("operations") || "[]");
-    const filteredOperations = operations.filter(op => op.id !== operationId);
-    localStorage.setItem("operations", JSON.stringify(filteredOperations));
   };
 
   return (
     <Card>
-      <Card.Header as="h5">Operation List</Card.Header>
+      <Card.Header>
+        <Card.Title>Operation List</Card.Title>
+      </Card.Header>
       <Card.Body>
-        <Table striped bordered hover>
+        <Table striped hover>
           <thead>
             <tr>
               <th>#</th>
               <th>Operation Code</th>
               <th>Name</th>
-              <th>Action</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
             {operationList.map((item, index) => (
-              <tr key={index}>
+              <tr key={item.id}>
                 <td>{index + 1}</td>
-                <td>{item.operation_code}</td>
-                <td>{item.name}</td>
+                <td>{item.operations.operation_code}</td>
+                <td>{item.operations.name}</td>
                 <td>
-                  <Button variant="danger" onClick={() => handleDelete(item.id)}>
+                  <Button
+                    className="btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
                     Delete
                   </Button>
                 </td>
