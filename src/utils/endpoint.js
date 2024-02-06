7095fc785862da5e4dbe59baa248130d3ece3faf
kevlei01001100 <156448718+kevlei01001100@users.noreply.github.com>
2024-02-06 16:14:28 +0000

    change endpoint

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,3 +1,4 @@
-const endpoint = "https://gsm-app.vercel.app";
+// const endpoint = "https://gsm-app.vercel.app";
+const endpoint = "https://gsm-fjjmnt6d1-daniels-projects-669b9b1b.vercel.app";
 // const endpoint = "http://127.0.0.1:8000";
 export default endpoint;
