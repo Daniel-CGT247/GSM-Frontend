d6a8e1d44beb828d915daf202f4188ff9717a622
Daniel-Tran-247 <baophuc247@gmail.com>
2024-02-08 17:49:20 -0500

    Add Skeleton to operation page

---
+++ src/components/TableSkeleton.jsx
@@ -0,0 +1,36 @@
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+
+export default function TableSkeleton({ header, columns }) {
+  const rows = [1, 2, 3, 4, 5];
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title>{header}</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Table striped hover>
+          <thead>
+            <tr>
+              {columns.map((col) => (
+                <th key={col} scope="col">{col}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr key={row}>
+                {columns.map((col) => (
+                  <td key={col}>
+                    <Skeleton mt={4} />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
+  );
+}
