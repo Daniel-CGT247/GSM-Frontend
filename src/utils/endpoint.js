1953060cf354516e838a14d4568d669e220932e7
Daniel-Tran-247 <baophuc247@gmail.com>
2024-02-09 15:04:07 -0500

    Change the endpoint to vercel link

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,4 +1,3 @@
-// const endpoint = "https://gsm-app.vercel.app";
-const endpoint = "https://gsm-fjjmnt6d1-daniels-projects-669b9b1b.vercel.app";
+const endpoint = "https://gsm-app.vercel.app";
 // const endpoint = "http://127.0.0.1:8000";
 export default endpoint;
