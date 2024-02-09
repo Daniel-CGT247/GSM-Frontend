27a78896ac6bc99ea4191412196cd8aa68ab6797
Kevin Lei <92414876+kev01001100@users.noreply.github.com>
2024-02-09 15:25:19 -0500

    updated yourlist.jsx

--- src/pages/YourListTable.jsx
+++ src/pages/YourListTable.jsx
@@ -1,60 +1,35 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
-import { Link } from 'react-router-dom';
-import endpoint from "../utils/endpoint";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import { Link } from "react-router-dom";
 
 export default function YourListTable({ bundleId, listId }) {
   const [operationList, setOperationList] = useState([]);
 
   const tableStyle = {
-    maxWidth: '80%',
-    margin: '0 auto',
+    maxWidth: "80%",  
+    margin: "0 auto",  
   }
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `${endpoint}/operation_list/`, 
+          `http://127.0.0.1:8000/operation_list/`,
           {
             params: {
               bundle_group: bundleId,
               listId: listId,
             },
             headers: {
-              Authorization: `JWT ${localStorage.getItem('access_token')}`,
+              Authorization: `JWT ${localStorage.getItem("access_token")}`,
             },
           }
         );
-
-        const operations = response.data
-        
-        const operationCodesResponse = await axios.get(
-          `${endpoint}/operation_code/`, 
-          {
-            headers: {
-              Authorization: `JWT ${localStorage.getItem('access_token')}`,
-            },
-          }
-        );
-
-        const operationCodes = operationCodesResponse.data
-
-        const operationsWithCodes = operations.map((operation) => {
-          const operationCodeInfo = operationCodes.find(
-            (code) => code.id === (operation.expanding_field ? operation.expanding_field.id : operation.operations.id)
-          );
-          return {
-            ...operation,
-            operation_code: operationCodeInfo ? operationCodeInfo.operation_code : '',
-          };
-        });
-
-        setOperationList(operationsWithCodes);
+        setOperationList(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -67,7 +42,7 @@ export default function YourListTable({ bundleId, listId }) {
         <thead>
           <tr>
             <th>Operation Code</th>
-            <th>Operation</th>
+            <th>Name</th>
             <th>Concat Name</th>
             <th>Total SAM</th>
             <th># of Elements</th>
@@ -77,17 +52,17 @@ export default function YourListTable({ bundleId, listId }) {
         <tbody>
           {operationList.map((item) => (
             <tr key={item.id}>
-              <td>{item.operation_code}</td>
+              <td>{item.expanding_field ? item.expanding_field.operation_code : ''}</td>
               <td>{item.operations.name}</td>
-              <td>{item.expanding_field ? item.expanding_field.name : item.operations.name}</td>
+              <td>{item.expanding_field ? item.expanding_field.name : ''}</td>
               <td>{Number(item.total_sam).toFixed(2)}</td>
               <td>{item.element_count}</td>
               <td>
-                  <Link
-                    to={`/${listId}/operation/${item.operations.id}/${item.id}/element`}
-                  >
-                    <Button variant="success">Build Elements</Button>
-                  </Link>
+                <Link
+                  to={`/${listId}/operation/${item.operations.id}/${item.id}/element`}
+                >
+                  <Button variant="success">Build Elements</Button>
+                </Link>
               </td>
             </tr>
           ))}
