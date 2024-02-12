3518e4db05fabb22ed78c6bb7876d7aff5e56013
Kevin Lei <92414876+kev01001100@users.noreply.github.com>
2024-02-12 14:05:46 +0000

    fixed operation page

--- src/pages/YourListTable.jsx
+++ src/pages/YourListTable.jsx
@@ -1,3 +1,95 @@
+// import {
+//   Button,
+//   Table,
+//   TableContainer,
+//   Tbody,
+//   Td,
+//   Th,
+//   Thead,
+//   Tr,
+//   Card,
+//   CardBody,
+//   TableCaption,
+//   Text,
+// } from "@chakra-ui/react";
+// import TableSkeleton from "../components/TableSkeleton";
+// import useGet from "../customed_hook/useGet";
+// import endpoint from "../utils/endpoint";
+
+// const columns = [
+//   "Name",
+//   "Description",
+//   "Job #",
+//   "Total SAM",
+//   "# of Elements",
+//   "",
+// ];
+// export default function YourListTable({ bundleId, listId }) {
+//   const params = {
+//     bundle_group: bundleId,
+//     listId: listId,
+//   };
+
+//   const { data: operationList, isLoading } = useGet(
+//     `${endpoint}/operation_list/`,
+//     params
+//   );
+//   return (
+//     <>
+//       {isLoading ? (
+//         <TableSkeleton header="Your List" columns={columns} />
+//       ) : (
+//         <Card>
+//           <CardBody>
+//             <TableContainer>
+//               <Table variant="striped" colorScheme="gray">
+//                 <TableCaption placement="top" bgColor="gray.50">
+//                   <Text as="h4">Your List</Text>
+//                 </TableCaption>
+//                 <Thead>
+//                   <Tr>
+//                     <Th>Name</Th>
+//                     <Th>Description</Th>
+//                     <Th>Job #</Th>
+//                     <Th isNumeric>Total SAM</Th>
+//                     <Th isNumeric># of Elements</Th>
+//                     <Th></Th>
+//                   </Tr>
+//                 </Thead>
+//                 <Tbody>
+//                   {operationList.map((item) => (
+//                     <Tr key={item.id}>
+//                       <Td>{item.operations.name}</Td>
+//                       <Td>
+//                         {item.expanding_field && item.expanding_field.name}
+//                       </Td>
+//                       <Td>
+//                         {item.expanding_field &&
+//                           item.expanding_field.operation_code}
+//                       </Td>
+//                       <Td isNumeric>{Number(item.total_sam).toFixed(2)}</Td>
+//                       <Td isNumeric>{item.element_count}</Td>
+//                       <Td>
+//                         <Button
+//                           as="a"
+//                           href={`/${listId}/operation/${item.operations.id}/${item.id}/element`}
+//                           colorScheme="twitter"
+//                         >
+//                           Build Elements
+//                         </Button>
+//                       </Td>
+//                     </Tr>
+//                   ))}
+//                 </Tbody>
+//               </Table>
+//             </TableContainer>
+//           </CardBody>
+//         </Card>
+//       )}
+//     </>
+//   );
+// }
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   Table,
@@ -11,10 +103,10 @@ import {
   CardBody,
   TableCaption,
   Text,
-} from "@chakra-ui/react";
-import TableSkeleton from "../components/TableSkeleton";
-import useGet from "../customed_hook/useGet";
-import endpoint from "../utils/endpoint";
+} from '@chakra-ui/react';
+import TableSkeleton from '../components/TableSkeleton';
+import useGet from '../customed_hook/useGet';
+import endpoint from '../utils/endpoint';
 
 const columns = [
   "Name",
@@ -24,20 +116,62 @@ const columns = [
   "# of Elements",
   "",
 ];
-export default function YourListTable({ bundleId, listId }) {
-  const params = {
-    bundle_group: bundleId,
-    listId: listId,
+
+const YourListTable = ({ bundleId, listId }) => {
+  const [operationCodes, setOperationCodes] = useState([]);
+  const [mappedOperationList, setMappedOperationList] = useState([]);
+
+
+  const { data: operationList, isLoading: isLoadingOperationList } = useGet(
+    `${endpoint}/operation_list/`,
+    { bundle_group: bundleId, listId: listId }
+  );
+
+
+  const fetchOperationCodes = async () => {
+    const response = await useGet(`${endpoint}/operation_code/`)
+    setOperationCodes(response.data || [])
   };
 
-  const { data: operationList, isLoading } = useGet(
-    `${endpoint}/operation_list/`,
-    params
-  );
+  useEffect(() => {
+    fetchOperationCodes();
+  }, [])
+
+
+  useEffect(() => {
+    if (operationList && operationCodes.length > 0) {
+      const newList = operationList.map((operation) => {
+        const operationCodeEntry = operationCodes.find(code => code.id === operation.operations.id)
+        
+        let operationCode = '';
+
+        if (operationCodeEntry && operationCodeEntry.name !== '') {
+          if (!operation.expanding_field) 
+          {
+            operationCode = '';
+          } else 
+          {
+            const expandingFieldCodeEntry = operationCodes.find(code => code.id === operation.expanding_field.id)
+            operationCode = expandingFieldCodeEntry ? expandingFieldCodeEntry.operation_code : '';
+          }
+        } else if (operationCodeEntry) {
+          operationCode = operationCodeEntry.operation_code
+        }
+
+        return {
+          ...operation,
+          operationCode: operationCode,
+        }
+      });
+
+      setMappedOperationList(newList)
+    }
+  }, [operationList, operationCodes]);
+
   return (
     <>
-      {isLoading ? (
-        <TableSkeleton header="Your List" columns={columns} />
+      {isLoadingOperationList ? (
+        <TableSkeleton header="Your List" columns={columns}/>
       ) : (
         <Card>
           <CardBody>
@@ -57,16 +191,11 @@ export default function YourListTable({ bundleId, listId }) {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {operationList.map((item) => (
+                  {mappedOperationList.map((item) => (
                     <Tr key={item.id}>
                       <Td>{item.operations.name}</Td>
-                      <Td>
-                        {item.expanding_field && item.expanding_field.name}
-                      </Td>
-                      <Td>
-                        {item.expanding_field &&
-                          item.expanding_field.operation_code}
-                      </Td>
+                      <Td>{item.expanding_field ? item.expanding_field.name : ''}</Td>
+                      <Td>{item.operationCode}</Td>
                       <Td isNumeric>{Number(item.total_sam).toFixed(2)}</Td>
                       <Td isNumeric>{item.element_count}</Td>
                       <Td>
@@ -89,3 +218,5 @@ export default function YourListTable({ bundleId, listId }) {
     </>
   );
 }
+
+export default YourListTable;
