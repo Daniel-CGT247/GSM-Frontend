faf1203097315edb59e53a8ec382ca9ed1f8fe70
Kevin Lei <92414876+kev01001100@users.noreply.github.com>
2024-02-16 08:59:55 -0500

    added jobgroup page instead of job group sidebar

---
+++ src/pages/JobGroup.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import { Link, useParams } from "react-router-dom";
+import JobGroupCard from "../components/JobGroupCard";
+import useGet from "../customed_hook/getData";
+import axios from "axios";
+export default function JobGroup() {
+  const { listId } = useParams();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/job_group/", {
+          params: { listId: listId },
+        });
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [listId]);
+  
+  const styleNum = useGet(`http://127.0.0.1:8000/collection/${listId}`);
+  const itemName = styleNum && styleNum.item && styleNum.item.name;
+
+return (
+    <div className="p-5">
+      <div className="flex flex-col items-center justify-center space-y-2">
+        <h1 className="font-bold">Job Group - Bundle Group</h1>
+        <h3 className="font-bold">Style {itemName}</h3>
+        <Link to="/Collection">
+          <Button variant="outline-secondary">Back to Collection</Button>
+        </Link>
+      </div>
+      <div className="flex justify-center flex-wrap gap-10 my-5">
+        {data.map((job_group) => (
+          <JobGroupCard job_group={job_group} listId={listId} />
+        ))}
+      </div>
+    </div>
+  );
+}
